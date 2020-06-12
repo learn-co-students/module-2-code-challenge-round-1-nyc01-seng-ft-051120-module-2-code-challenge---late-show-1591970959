cc959b6bde33aec56f0f64b1b959cd89968bb0d6
grantkoji <grantkoji@gmail.com>
2020-06-12 12:05:40 -0400

    Final draft again with commenting out code that doesn't work forr bonus deliverable

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -3,14 +3,14 @@ class Appearance < ApplicationRecord
     belongs_to :episode
     validates :numeric_rating, numericality: { greater_than_or_equal_to: 1 }
     validates :numeric_rating, numericality: { less_than_or_equal_to: 5 }
-    validate :guest_one_appearance_per_ep
+    # validate :guest_one_appearance_per_ep
 
-    private
+    # private
 
-    def guest_one_appearance_per_ep
-        byebug
-        if self.episode.guests != self.episode.guests.uniq
-            self.errors.add = "This Guest has already been booked for this Episode"
-        end
-    end
+    # def guest_one_appearance_per_ep
+    #     byebug
+    #     if self.episode.guests != self.episode.guests.uniq
+    #         self.errors.add = "This Guest has already been booked for this Episode"
+    #     end
+    # end
 end
