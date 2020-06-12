6d3aad75ce8431d3a89f1dbc94a2c1113ac3e2c8
rmcclorey1125 <rmcclorey1@yahoo.com>
2020-06-12 12:03:48 -0400

    Final

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -3,6 +3,8 @@ class Appearance < ApplicationRecord
     belongs_to :episode
 
     validates :numeric_rating, inclusion: { in: (1 .. 5)}
+    validates :guest_id, uniqueness: { scope: :episode_id }
+
 
     def date_rating
         "Date: #{self.episode.date} - Rating: #{self.numeric_rating}"
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,13 @@
 class Episode < ApplicationRecord
     has_many :appearances
     has_many :guests, through: :appearances
+
+    def average_rating
+        average = 0
+        self.appearances.each do |rate|
+            average += rate.numeric_rating
+        end
+        average/appearances.count
+    end
+
 end
