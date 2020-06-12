9456749f53f03de3ca17e588d603bf149a757fee
chloekim917 <chloekim917@gmail.com>
2020-06-12 11:48:37 -0400

    deliverables

---
+++ app/controllers/appearances_controller.rb
@@ -0,0 +1,24 @@
+class AppearancesController < ApplicationController
+    def new
+        @appearance = Appearance.new
+
+        @guests = Guest.all
+        @episodes = Episode.all
+    end
+
+    def create
+        appearance = Appearance.create(appearance_params)
+        if appearance.valid?
+            redirect_to episode_path(appearance.episode_id)
+        else
+            flash[:appearance_errors] = appearance.errors.full_messages
+            redirect_to new_appearance_path
+        end
+    end
+
+    private
+
+    def appearance_params
+        params.require(:appearance).permit(:episode_id, :guest_id, :rating)
+    end
+end
