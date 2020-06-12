d9c29de2ff31e8fce16dc4a8ad19e411ebd72203
grantkoji <grantkoji@gmail.com>
2020-06-12 11:46:11 -0400

    guest appearances oordered by rating delivered

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -5,4 +5,11 @@ class Guest < ApplicationRecord
     def name_and_occupation
         "Name: #{self.name} - Occupation: #{self.occupation}"
     end
+
+    def order_by_rating
+        self.appearances.sort do |appearance1, appearance2|
+            appearance2.numeric_rating <=> appearance1.numeric_rating
+        end
+    end
+
 end
