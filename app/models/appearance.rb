dfd9ecdfc2e5ea4ab4a3ca8950939f6159b21e49
steveneross94 <ross.steven@me.com>
2020-06-12 11:57:32 -0400

    7 out of 8 deliverables

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -3,4 +3,6 @@ class Appearance < ApplicationRecord
     belongs_to :episode
     validates :rating, numericality: {less_than_or_equal_to: 5, greater_than_or_equal_to: 1}
     validates :episode_id, uniqueness: {scope: :guest_id, message: "That guest has already been on the air"}
+
+    
 end
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,16 @@
 class Episode < ApplicationRecord
     has_many :appearances
     has_many :guests, through: :appearances
+
+    def avg_rating
+        ratings = []
+        self.appearances.each do |e|
+            all_ratings= [] 
+            all_ratings << e.rating
+            average = (all_ratings.sum(0.0) / all_ratings.size)
+            ratings << average
+        end
+        average_ratings = (ratings.sum(0.0) / ratings.size)
+        average_ratings
+    end
 end
