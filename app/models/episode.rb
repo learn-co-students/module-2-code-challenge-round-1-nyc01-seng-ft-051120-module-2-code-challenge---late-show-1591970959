512c83c806059cfa50fd8e4347f4661cb8163ae2
Gary Cordero <GaryCordero1690@gmail.com>
2020-06-12 11:49:48 -0400

    average rating added to show page

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,5 +2,12 @@ class Episode < ApplicationRecord
   has_many :appearances
   has_many :guests, through: :appearances
 
- 
+  def average_rating 
+    if self.appearances.empty?
+      return 0
+    end 
+    sum = self.appearances.reduce(0){|sum, appearance| sum + appearance.rating}
+    average = sum / self.appearances.count
+  end
 end
+
