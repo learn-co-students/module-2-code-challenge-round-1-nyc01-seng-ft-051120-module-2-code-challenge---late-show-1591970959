af2d677363503befaf655a734445556a24700878
github username <github email address>
2020-06-12 12:05:47 -0400

    AndrewCodeChallenge

---
+++ app/controllers/appearances_controller.rb
@@ -0,0 +1,28 @@
+class AppearancesController < ApplicationController
+    
+    def new
+        @appearance = Appearance.new
+
+        @episodes = Episode.all
+        @guests = Guest.all
+    end
+
+    def create
+        appearance = Appearance.create(appearance_params)
+
+        if appearance.valid?
+            redirect_to episode_path(appearance.episode_id)
+        else
+            flash[:appearance_errors] = appearance.errors.full_messages
+            redirect_to new_appearance_path
+        end
+    end
+
+
+    private
+
+    def appearance_params
+        params.require(:appearance).permit(:episode_id, :guest_id, :rating)
+    end
+
+end
